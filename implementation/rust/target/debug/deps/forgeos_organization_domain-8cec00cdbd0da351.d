89c9cd809db1bdda03576ab5eb2f39d6a6e8560f
Setiawan-Hashmicro <signiswellfire@gmail.com>
2026-08-01 17:45:52 +0800

    chore: validate rust workspace initialization

---
+++ implementation/rust/target/debug/deps/forgeos_organization_domain-8cec00cdbd0da351.d
@@ -0,0 +1,5 @@
+C:\Users\ricky\OneDrive\Documents\GitHub\forgeos\implementation\rust\target\debug\deps\forgeos_organization_domain-8cec00cdbd0da351.d: domains\organization-domain\src\lib.rs
+
+C:\Users\ricky\OneDrive\Documents\GitHub\forgeos\implementation\rust\target\debug\deps\libforgeos_organization_domain-8cec00cdbd0da351.rmeta: domains\organization-domain\src\lib.rs
+
+domains\organization-domain\src\lib.rs:
---
+++ implementation/rust/target/debug/deps/forgeos_organization_infrastructure-05c6138de7859ae2.d
@@ -0,0 +1,5 @@
+C:\Users\ricky\OneDrive\Documents\GitHub\forgeos\implementation\rust\target\debug\deps\forgeos_organization_infrastructure-05c6138de7859ae2.d: infrastructure\organization\src\lib.rs
+
+C:\Users\ricky\OneDrive\Documents\GitHub\forgeos\implementation\rust\target\debug\deps\libforgeos_organization_infrastructure-05c6138de7859ae2.rmeta: infrastructure\organization\src\lib.rs
+
+infrastructure\organization\src\lib.rs:
